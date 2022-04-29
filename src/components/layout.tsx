5a137e63a437646aeb92dc8a1b9877e5061826d6
tommyxchow <tommyxchow@gmail.com>
2022-04-28 22:11:45 -0400

    Update favicons

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -9,7 +9,36 @@ const Layout = (props: LayoutProps) => {
         <title>Frosty</title>
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
         <meta name='description' content={props.description} />
-        <link rel='icon' href='/logo.svg' />
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/favicons/apple-touch-icon.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/favicons/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicons/favicon-16x16.png'
+        />
+        <link rel='manifest' href='/favicons/site.webmanifest' />
+        <link
+          rel='mask-icon'
+          href='/favicons/safari-pinned-tab.svg'
+          color='#5bbad5'
+        />
+        <link rel='shortcut icon' href='/favicons/favicon.ico' />
+        <meta name='msapplication-TileColor' content='#603cba' />
+        <meta
+          name='msapplication-config'
+          content='/favicons/browserconfig.xml'
+        />
+        <meta name='theme-color' content='#ffffff' />
       </Head>
 
       <main className='flex min-h-screen flex-col items-center p-8'>
