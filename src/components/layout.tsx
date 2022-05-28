67319da27efa271609fd7b60ea0043157128f6c6
Tommy Chow <tommyxchow@gmail.com>
2022-05-28 02:48:38 -0400

    Update app, document, and fonts

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -6,39 +6,7 @@ const Layout = (props: LayoutProps) => {
   return (
     <div className='bg-gradient-to-b from-twitch-purple to-black'>
       <Head>
-        <title>Frosty</title>
-        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
         <meta name='description' content={props.description} />
-        <link
-          rel='apple-touch-icon'
-          sizes='180x180'
-          href='/favicons/apple-touch-icon.png'
-        />
-        <link
-          rel='icon'
-          type='image/png'
-          sizes='32x32'
-          href='/favicons/favicon-32x32.png'
-        />
-        <link
-          rel='icon'
-          type='image/png'
-          sizes='16x16'
-          href='/favicons/favicon-16x16.png'
-        />
-        <link rel='manifest' href='/favicons/site.webmanifest' />
-        <link
-          rel='mask-icon'
-          href='/favicons/safari-pinned-tab.svg'
-          color='#5bbad5'
-        />
-        <link rel='shortcut icon' href='/favicons/favicon.ico' />
-        <meta name='msapplication-TileColor' content='#603cba' />
-        <meta
-          name='msapplication-config'
-          content='/favicons/browserconfig.xml'
-        />
-        <meta name='theme-color' content='#ffffff' />
       </Head>
 
       <main className='flex min-h-screen flex-col items-center p-8 pt-24'>
