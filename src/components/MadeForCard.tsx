4165e1183cf3801dcef7243aab337f6ccc03ff98
Tommy Chow <tommyxchow@gmail.com>
2022-08-15 23:14:56 -0400

    Adjust home layout and tweaks

--- src/components/MadeForCard.tsx
+++ src/components/MadeForCard.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function MadeForCard({ icon, user, description }: WhyCardProps) {
   return (
-    <div className='flex w-full flex-col gap-2 rounded-lg bg-neutral-800 p-8 lg:bg-transparent'>
+    <div className='flex w-full flex-col gap-2 rounded-lg bg-neutral-800 p-8'>
       <div className='flex items-center gap-2'>
         <div className='text-2xl'>{icon}</div>
         <h2 className='text-xl font-semibold lg:text-2xl'>{user}</h2>
