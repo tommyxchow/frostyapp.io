7de21494932de672e84b263c8a42318dd9838100
Tommy Chow <tommyxchow@gmail.com>
2022-08-17 00:40:21 -0400

    Add custom 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const Custom404 = () => {
+  return (
+    <Layout title='404' description='Page not found'>
+      <div className='flex min-h-screen w-full items-center justify-center text-neutral-400'>
+        404 | Page not found
+      </div>
+    </Layout>
+  );
+};
+
+export default Custom404;
