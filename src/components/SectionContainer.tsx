2165893d9bbfc90d2a41d3c6489bdd5c9e150a9e
Tommy Chow <tommyxchow@gmail.com>
2022-08-16 20:11:06 -0400

    Polish layout, breakpoints, and content

--- src/components/SectionContainer.tsx
+++ src/components/SectionContainer.tsx
@@ -4,11 +4,18 @@ const SectionContainer = ({
   className,
   header,
   children,
+  centerHeader = false,
 }: SectionContainerProps) => {
   return (
-    <section className={`w-full max-w-screen-xl ${className}`}>
+    <section
+      className={`w-full max-w-screen-xl py-8 px-4 lg:py-16 ${className}`}
+    >
       {header && (
-        <h1 className='p-4 text-center text-lg font-bold uppercase tracking-wider text-purple-500 lg:p-8 lg:text-xl'>
+        <h1
+          className={`p-4 text-center text-xl font-bold lg:p-8 lg:text-3xl ${
+            !centerHeader && 'lg:text-left'
+          }`}
+        >
           {header}
         </h1>
       )}
@@ -21,6 +28,7 @@ interface SectionContainerProps {
   className?: string;
   children: React.ReactNode;
   header?: string;
+  centerHeader?: boolean;
 }
 
 export default SectionContainer;
